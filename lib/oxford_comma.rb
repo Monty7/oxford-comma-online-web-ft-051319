96140c705af3c01a3cc74720350d9450dd8e9a35
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 20:15:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   case array.size
     when 1
-      array.str
+      array.to_str
     when 2 
-      
+      array.join(" and ")
     when 3
-      
+    
   end
   array.join(" and ");
 end
