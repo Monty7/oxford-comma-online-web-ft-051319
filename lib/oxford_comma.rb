8e1ca364206112a964798c83417a4d1f66b3403f
voluble-bug-0219 <montensia1@outlook.com>
2019-05-14 21:02:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
       array.join
     when 2 
       array.join(" and ")
-    when 3
+    when > 3
       last_string = array.pop
       joinedString = array.join(", ")
       "#{joinedString}, and #{last_string}"
